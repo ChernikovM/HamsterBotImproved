57d14f3fcc81339c4526a758f3c6a433bb5e44e0
Maksym Chernikov <mmcv@emagine.org>
2024-04-17 15:56:34 +0300

    many changes, bugs fixed, unstable

---
+++ bot/core/user_agents.py
@@ -0,0 +1,9 @@
+user_agents = [
+        'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; U; Android 7.0; Nexus 6X Build/NPD90G) AppleWebKit/600.10 (KHTML, like Gecko)  Chrome/51.0.3391.325 Mobile Safari/533.5',
+        'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.118 Mobile Safari/537.36',
+        'Mozilla/5.0 (Android 14; Mobile; LG-M255; rv:125.0) Gecko/125.0 Firefox/125.0',
+        'Mozilla/5.0 (Linux; Android 14; LM-Q720) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.118 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 14; LM-X420) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.118 Mobile Safari/537.36',
+        'Mozilla/5.0 (Linux; Android 14; LM-Q710(FGN)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.118 Mobile Safari/537.36'
+]
