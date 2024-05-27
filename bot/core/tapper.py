672aefb2314f68919d7d57f8949fb6de520529dc
Maksym Chernikov <mmcv@emagine.org>
2024-05-27 21:42:25 +0300

    colldown added

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -422,7 +422,7 @@ class Tapper:
                             
                             while True:
                                 best_upgrade = max(available_upgrades, key=lambda x: (x["profitPerHourDelta"] / x["price"]) if x["price"] != 0 else float('-inf'))
-                                time_to_earn = (best_upgrade["price"] - balance) / PLAYER_DATA_HOURLY_EARNINGS
+                                time_to_earn = best_upgrade["cooldownSeconds"] != 0 ? best_upgrade["cooldownSeconds"] : (best_upgrade["price"] - balance) / PLAYER_DATA_HOURLY_EARNINGS
                                 time_to_return = int(best_upgrade["price"]/best_upgrade["profitPerHourDelta"])
                                 logger.info(f"{self.session_name} | Best upgrade for now: <e>{best_upgrade['id']}</e> | <g>+{best_upgrade['profitPerHourDelta']}</g> | price:<b>{best_upgrade['price']}</b> | TTR: <b>{time_to_return}</b>")
                                 await asyncio.sleep(delay=1)
